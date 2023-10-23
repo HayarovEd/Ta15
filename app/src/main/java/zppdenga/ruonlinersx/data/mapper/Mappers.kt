95a105fd201ee644049597bbbcb3c02b48b149cd
hayarovy <edurd@rambler.ru>
2023-10-23 22:12:54 +0500

    5

--- app/src/main/java/zppdenga/ruonlinersx/data/mapper/Mappers.kt
+++ app/src/main/java/zppdenga/ruonlinersx/data/mapper/Mappers.kt
@@ -1,5 +1,9 @@
 package zppdenga.ruonlinersx.data.mapper
 
+import android.os.Build.VERSION_CODES
+import androidx.annotation.RequiresApi
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import zppdenga.ruonlinersx.data.remote.LoanDto
 import zppdenga.ruonlinersx.domain.model.Loan
 
@@ -17,12 +21,21 @@ fun List<LoanDto>.mapToLoan(): List<Loan> {
 
 fun String.convertToInt(): Int {
     return try {
-        (this.substring(3,5)+this.substring(6)).toInt()
-    } catch (e:Exception) {
+        (this.substring(3, 5) + this.substring(6)).toInt()
+    } catch (e: Exception) {
         1000000
     }
 }
 
-fun maxAmount(data:List<Loan>): Int {
+fun maxAmount(data: List<Loan>): Int {
     return data.maxOf { it.sumOneInt }
+}
+
+@RequiresApi(VERSION_CODES.O)
+fun setTime(): String {
+    val localTime = LocalTime.now()
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+    return localTime
+        .plusMinutes(30)
+        .format(formatter)
 }
